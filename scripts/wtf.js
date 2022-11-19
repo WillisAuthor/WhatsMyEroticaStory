4ed382b349f8313509be68fd174966e89d202144
WillisAuthor <100448459+WillisAuthor@users.noreply.github.com>
2022-11-19 14:38:27 -0500

    Update wtf.js

--- scripts/wtf.js
+++ scripts/wtf.js
@@ -13,12 +13,13 @@ var WTF = (function() {
 
     var RE_QUOTE = /\"([^\"]+)\"/gi;
     var RE_JSON = /\.json$/i;
+    var RE_COL = /^gsx\$(.+)$/i;
     var RE_KEY = /[a-z0-9_-]{32,}/i;
-    var DOCS_PATH = 'https://docs.google.com/spreadsheet/pub?key={key}&output=csv';
+    var DOCS_PATH = "https://spreadsheets.google.com/feeds/list/{key}/od6/public/values?alt=json";
+
 
     var templates;
     var responses;
-	var books;
     var headings;
     var corpus;
     var regex;
@@ -39,18 +40,46 @@ var WTF = (function() {
         templates = corpus.template;
         responses = corpus.response;
         headings = corpus.heading;
-		books = corpus.books;
 
         delete corpus.template;
         delete corpus.response;
         delete corpus.heading;
-		delete corpus.books;
 
         // Enable UI and generate first idea
 
         initUI();
         buildRexExp();
         generate();
+        $('#templates').html(templates.length + ' template' + (templates.length == 1 ? '' : 's'))
+        $('#outcomes').html(getTotalNumOptions().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+    }
+
+    function getTotalNumOptions() {
+        var num = 0
+        for(var i = 0; i < templates.length; i++) {
+            num += getNumTemplateOptions(templates[i])
+        }
+        return num
+    }
+
+    function getNumTemplateOptions( template ) {
+        var type, iter = 0, // Safety mechanism
+            item = regex.exec( template ),
+            copy = cloneCorpus();
+
+        var num = 0
+        while ( item && ++iter < 1000 ) {
+            type = item[ 0 ].substr(1);
+            var typeNum = corpus[type].length
+            if(num == 0) {
+                num = corpus[type].length
+            }
+            else {
+                num = num * corpus[type].length
+            }
+            item = regex.exec( template );
+        }
+        return num
     }
 
     /*
@@ -63,8 +92,6 @@ var WTF = (function() {
     */
 
     function parseCSV( csv ) {
-
-        var corpus = {};
 
         var i, j, k, n, m, cols, keys = {}, data = {}, rows = csv.split( '\n' );
 
@@ -81,6 +108,46 @@ var WTF = (function() {
                 } else if ( cols[ k ] ) {
 
                     data[ keys[ k ] ][ j ] = unescape( cols[ k ] ).replace( /^\"|\"$/g, '' );
+                }
+            }
+        }
+
+        return data;
+    }
+
+    /*
+      ------------------------------------------------------------
+
+        Converts JSON data to a regular corpus object
+        @see sample.json
+
+      ------------------------------------------------------------
+    */
+
+    function parseJSON( json ) {
+
+        var i, n, key, val, map = {}, keys = {}, data = {}, rows = json.feed.entry;
+
+        for ( key in rows[0] ) {
+            
+            if ( RE_COL.test( key ) ) {
+                
+                map[ key ] = key.match( RE_COL )[ 1 ].toLowerCase();
+                keys[ key ] = [];
+            }
+        }
+
+        for ( key in keys ) {
+            
+            data[ map[ key ] ] = keys[ key ];
+
+            for ( i = 0, n = rows.length; i < n; i++ ) {
+
+                val = rows[ i ][ key ].$t;
+
+                if ( val && val.length ) {
+
+                    keys[ key ].push( val );
                 }
             }
         }
@@ -127,6 +194,13 @@ var WTF = (function() {
 
             types.push( type );
 
+        types = types.sort(function (a, b) {
+            if (a.length == b.length) {
+                return 0
+            }
+            return a.length > b.length ? -1 : 1
+        })
+        
         var content = '@(type)'.replace( 'type', types.join( '|' ) );
 
         regex = new RegExp( content, 'gi' );
@@ -151,8 +225,6 @@ var WTF = (function() {
 
             type = item[ 0 ];
             text = item[ 1 ];
-
-            console.log( text, copy, copy[ text ] );
 
             part = randomItem( copy[ text ], true );
             idea = idea.replace( type, part );
@@ -264,12 +336,12 @@ var WTF = (function() {
 
                 } else if ( RE_KEY.test( data ) ) {
 
-                    // CSV
+                    // JSON
 
                     $.ajax({
                         url: DOCS_PATH.replace( '{key}', data ),
                         success: function( data, status, xhr ) {
-                            corpus = parseCSV( data );
+                            corpus = parseJSON( data );
                             start();
                         },
                         error: function( xhr, errorType, error ) {
@@ -297,3 +369,12 @@ var WTF = (function() {
     };
 
 })();
+
+
+const reloadtButton = document.querySelector("#reload");
+// Reload everything:
+function reload() {
+    reload = location.reload();
+}
+// Event listeners for reload
+reloadButton.addEventListener("click", reload, false);
